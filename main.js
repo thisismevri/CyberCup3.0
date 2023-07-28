a149610f019b48c59f0d18747ec9eefb50f18366
Raghav <raghavkumarsf@gmail.com>
2023-07-28 16:24:11 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const buttons = document.querySelectorAll(".faq-toggle");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () =>
+    button.parentElement.classList.toggle("active")
+  );
+});
+
+
